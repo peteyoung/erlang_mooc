8855637740030825191003b4f9b1213b5859a09d
Pete Young <pete@simpli.fi>
2017-03-27 19:14:43 -0500

    build_tuples and format_index_line working. matches instructions.

--- src/week2/doityourself/index.erl
+++ src/week2/doityourself/index.erl
@@ -1,7 +1,8 @@
 -module(index).
 -export([get_file_contents/1,show_file_contents/1,
         index/1, extract_word/1, extract_words/1,
-	parse/1]).
+	parse/1, get_next_num/2, build_tuples/1,
+	format_index_line/2]).
 
 % Used to read a file into a list of lines.
 % Example files available in:
@@ -43,11 +44,65 @@ show_file_contents([]) ->
 index(Name) ->
     Lines = get_file_contents(Name),
     WordsByLine = parse(Lines),
-    map_lines_to_words(WordsByLine).
+    LinesByWord = map_lines_to_words(WordsByLine),
+    format_index(LinesByWord).
 %% index:index("gettysburg-address.txt").
 %% D = index:index("gettysburg-address.txt").
 
 
+%% iterate dict of line nums keyed by word
+format_index(LinesByWord) ->
+    format_index(dict:fetch_keys(LinesByWord), LinesByWord, []).
+
+format_index([], _LinesByWord, Index) ->
+    Index;
+format_index([Word|More], LinesByWord, Index) ->
+    LNums = dict:fetch(Word),
+    format_index(More, LinesByWord, [format_index_line(Word, LNums)|Index]).
+
+
+%% format an entry as { "word", [{x,x}, {y,z}] }
+%% Word - word to be indexed
+%% LNums - list of line numbers word was found on
+format_index_line(Word, LNums) ->
+    { Word, build_tuples(LNums) }.
+%% index:format_index_line("baz", [1,2,3,4]).
+%% index:format_index_line("bar", [1,2,3,4,6,7,8,10]).
+%% index:format_index_line("foo", [3,4,5,7,11,12,13]).
+
+
+%% iterate list of line nums and get tuples for word
+build_tuples([]) -> [];
+build_tuples([Num]) -> [{Num,Num}];
+build_tuples([Start|More]) ->
+    {End, Rem} = get_next_num(Start, More),
+    [{Start,End} | build_tuples(Rem)].
+%% index:build_tuples([1,2,3,4]) == [{1,4}].
+%% index:build_tuples([1,2,3,4,6,7,8,10]) == [{1,4},{6,8},{10,10}].
+%% index:build_tuples([3,4,5,7,11,12,13]) == [{3,5},{7,7},{11,13}].
+
+
+%% iterate list of numbers and return start and end of any series
+%% as a tuple. e.g. 1,2,3,4 -> {1,4}.
+%% any non series number merely gets duplicated 1,5,6,7 -> {1,1}.
+%% any remaining numbers also get returned.
+get_next_num(Num, []) ->
+    {Num, []};
+get_next_num(Num, [H|T]) when Num == H; Num+1 == H ->
+    get_next_num(H, T);
+get_next_num(Num, [H|T]) ->
+    {Num, [H|T]}.
+%% index:get_next_num(1, [2,3,4,6]) == {4,[6]}.
+%% index:get_next_num(1, [3,4,5,6]) == {1,[3,4,5,6]}.
+%% index:get_next_num(1, [1,2,3,4,6]) == {4,[6]}.
+%% index:get_next_num(1, [1,2,3,4,5,6]) == {6,[]}.
+%% index:get_next_num(1, [1]) == {1,[]}.
+%% index:get_next_num(1, [2]) == {2,[]}.
+%% index:get_next_num(1, [3]) == {1,[3]}.
+
+
+
+%% iterate dict of line nums -> words and pivot to words -> line nums
 map_lines_to_words(WordsByLine) ->
     map_lines_to_words(WordsByLine, dict:new()).
 
@@ -60,9 +115,8 @@ map_lines_to_words([H|T], Dict) ->
 
 add_line_to_words(_, [], Dict) ->
     Dict;
-add_line_to_words(LineNum, [H|T], Dict) ->
-    %io:fwrite("append H:~s, #:~w, D:~w~n", [H, LineNum, Dict]).
-    add_line_to_words(LineNum, T, dict:append(H, LineNum, Dict)).
+add_line_to_words(LineNum, [Word|More], Dict) ->
+    add_line_to_words(LineNum, More, dict:append(Word, LineNum, Dict)).
 
 
 parse(Lines) -> parse(Lines, [], 1).
