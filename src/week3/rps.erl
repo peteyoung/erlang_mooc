208a3770900cbfc19bfe23f857452b4048d7a672
Pete Young <pete@simpli.fi>
2017-03-30 12:33:29 -0500

    first shot at rock paper scissors

---
+++ src/week3/rps.erl
@@ -0,0 +1,43 @@
+-module(rps).
+-export([beat/1, lose/1, result/2, tournament/2, test/0]).
+
+beat(rock) -> paper;
+beat(paper) -> scissors;
+beat(scissors) -> rock.
+
+lose(rock) -> scissors;
+lose(paper) -> rock;
+lose(scissors) -> paper.
+
+result(L, R) -> 
+    case L == lose(R) of
+        true  -> 1;
+        false -> case L == beat(R) of
+            true  -> -1;
+            false -> 0
+        end
+    end.
+
+results([], []) -> [];
+results([L|Ls], [R|Rs]) -> [result(L, R)|results(Ls, Rs)].
+
+tournament(L, R) ->
+    lists:foldr(fun (X, Acc) -> Acc + X end,
+                0,
+		results(L, R)).
+
+test() ->
+    1 = result(rock, paper),
+    1 = result(paper, scissors),
+    1 = result(scissors, rock),
+    0 = result(rock, rock),
+    0 = result(paper, paper),
+    0 = result(scissors, scissors),
+    -1 = result(rock, scissors),
+    -1 = result(paper, rock),
+    -1 = result(scissors, paper),
+
+    -1 = tournament([rock,rock,paper,paper],[rock,paper,scissors,rock],
+
+    ok.
+
